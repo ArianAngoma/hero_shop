6ae09a6d93f255be7382152a38ca32bd4b74403f
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-07-02 21:31:09 -0500

    MainContainer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from 'framer-motion'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
-import Logo from './img/logo.png'
-import Avatar from './img/avatar.png'
+import Logo from '../img/logo.png'
+import Avatar from '../img/avatar.png'
 
 import { app } from '../configs/firebase'
 import { useStateValue } from '../context/StateProvider'
@@ -160,6 +160,14 @@ export const Header = () => {
       </div>
 
       <div className="flex items-center justify-between md:hidden w-full h-full">
+        <div className="relative flex items-center justify-center">
+          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer"/>
+          <div
+            className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+        </div>
+
         <Link
           className="flex items-center gap-2"
           to={'/'}
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,31 @@
+import HeroShop from '../img/shop.png'
+
 export const MainContainer = () => {
   return (
-    <div>
-      <h1>Main Container</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+          <p className="text-base text-orange-500 font-semibold">Hero Shop</p>
+
+          <div className="w-6 h-6 bg-white rounded-full overflow-hidden drop-shadow-xl">
+            <img src={HeroShop} alt="heroShop" className="w-full h-full object-contain"/>
+          </div>
+        </div>
+
+        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          The best heroes store is in{' '}
+          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">Your Hands</span>
+        </p>
+
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Buy Now
+        </button>
+      </div>
+
+      <div className="py-2 bg-blue-400 flex-1"></div>
     </div>
   )
 }
