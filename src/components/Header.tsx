ec934cb00b1b9bb20309fdd608840b0346807be9
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-07-03 21:33:48 -0500

    Email authorized to add products

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,7 +105,7 @@ export const Header = () => {
                   }}
                 >
                   {
-                    user && user.email === 'arian.angoma.js@gmail.com' && (
+                    user && ['arian.angoma.js@gmail.com', 'achalarca@todo1.com', 'marcos@develative.com'].includes(user.email) && (
                       <Link to={'/createProduct'}>
                         <p
                           className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
@@ -180,7 +180,7 @@ export const Header = () => {
                 }}
               >
                 {
-                  user && user.email === 'arian.angoma.js@gmail.com' && (
+                  user && ['arian.angoma.js@gmail.com', 'achalarca@todo1.com', 'marcos@develative.com'].includes(user.email) && (
                     <Link to={'/createProduct'}>
                       <p
                         className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
